37953c51dfa9f35c2610161b22c9a32db8becd05
mmizzle9 <mmizzle9@gmail.com>
2014-03-22 10:36:43 -0600

    Added initial project

---
+++ Resume_App.Droid/Resume_App.Droid/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Resume_App.Droid
+{
+    [Activity(Label = "Resume_App.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            ActionBar.Title = "My Resume";
+        }
+    }
+}
